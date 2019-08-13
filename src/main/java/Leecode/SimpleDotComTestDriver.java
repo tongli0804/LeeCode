a84804ecdc5f92e7e326e47dac03b7850ab76e69
Tmint <tongli1992@hotmail.com>
2019-08-13 17:45:43 +0800

    7月17号

--- src/main/java/Leecode/SimpleDotComTestDriver.java
+++ src/main/java/Leecode/SimpleDotComTestDriver.java
@@ -1,5 +1,6 @@
 package Leecode;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class SimpleDotComTestDriver {
@@ -13,59 +14,83 @@ public class SimpleDotComTestDriver {
 //             x =(int) (Math.random()*10);
 //             loctions[i]=x;;
 //        }
-        for (int i=0;i<3;){
-           if(getShip(loctions)){
-            i++;
-           }
+        for (int i = 0; i < 3; ) {
+            if (getShip(loctions)) {
+                i++;
+            }
         }
 
-        Scanner  sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         dot.SetlocationCells(loctions);
-        while (true){
+        while (true) {
 
-        String userguess = sc.nextLine();
-        String result = dot.checkYourself(userguess);
-        if(result.equals("kill")){
-        break;
-        }
+            String userguess = sc.nextLine();
+            String result = dot.checkYourself(userguess);
+            if (result.equals("kill")) {
+                break;
+            }
         }
 
 
     }
-private static boolean setLoc(int[][] loctions,int head_h,int head_s){
-         if(loctions[head_h][head_s]==1){
-             return false;
-         }
-    loctions[head_h][head_s]=1;
-return true;
-}
-    private static boolean getShip(int[][] loctions) {
-        int head_h =(int) (Math.random()*5);
-        int head_s =(int) (Math.random()*5);
-        int dir =(int) (Math.random()*2);
-        if(!setLoc(loctions,head_h,head_s)){
+    //确定三个长方形位置
+
+    private static boolean setLoc(int[][] loctions, int head_x, int head_y) {
+        if (loctions[head_x][head_y] == 1) {
             return false;
         }
-        if(dir==0){
-            if(!setLoc(loctions,head_h+1,head_s)){
-                return false;
-            }
-            if(!setLoc(loctions,head_h+2,head_s)){
-                return false;
-            }
-        }else {
-            if(!setLoc(loctions,head_h,head_s+1)){
-                return false;
-            }
-            if(!setLoc(loctions,head_h,head_s+2)){
-                return false;
-            }
-        }
-    return true;
+        loctions[head_x][head_y] = 1;
+        return true;
     }
 
+    private static boolean getShip(int[][] loctions) {
+        Random r = new Random();
+        for (int i = 0; i < 3; i++) {
+            int head_x = r.nextInt(8);
+            int head_y = r.nextInt(8);
+            int dir = r.nextInt(4);
+            if (!setLoc(loctions, head_x, head_y)) {
+                return false;
+            }
+            //0-上   1-下   2-左   3-右
+            if (dir == 0) {
+                if (!setLoc(loctions, head_x, head_y + 1)) {
+                    return false;
+                }
+                if (!setLoc(loctions, head_x, head_y + 2)) {
+                    return false;
+                }
+            } else if (dir == 1) {
+                if (!setLoc(loctions, head_x, head_y - 1)) {
+                    return false;
+                }
+                if (!setLoc(loctions, head_x, head_y - 2)) {
+                    return false;
+                }
+            } else if (dir == 2) {
+                if (!setLoc(loctions, head_x - 1, head_y)) {
+                    return false;
+                }
+                if (!setLoc(loctions, head_x - 2, head_y)) {
+                    return false;
+                }
+            } else {
+                if (!setLoc(loctions, head_x + 1, head_y)) {
+                    return false;
+                }
+                if (!setLoc(loctions, head_x + 2, head_y)) {
+                    return false;
+                }
+            }
+            return true;
+        }
 
+        return false;
+    }
 }
+
+
+
 
 class SimpleDotCom{
     int[] locationCells;
