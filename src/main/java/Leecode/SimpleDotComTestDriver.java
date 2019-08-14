667294cb7f1580afdc41c7f805b9dea25afcb647
tongli <tongli1992@hotmail.com>
2019-08-15 00:05:13 +0800

    7月17号 gameove

--- src/main/java/Leecode/SimpleDotComTestDriver.java
+++ src/main/java/Leecode/SimpleDotComTestDriver.java
@@ -5,20 +5,18 @@ import java.util.Scanner;
 
 public class SimpleDotComTestDriver {
 
+
     public static void main(String[] args) {
         SimpleDotCom dot = new SimpleDotCom();
         //初始化战舰
         int[][] loctions = new int[7][7];
-//        for (int i = 0 ;i<loctions.length;i++){
-//            int x;
-//             x =(int) (Math.random()*10);
-//             loctions[i]=x;;
-//        }
-        for (int i = 0; i < 3; ) {
-            if (getShip(loctions)) {
-                i++;
+        for (int i = 0 ;i<loctions.length;i++){
+            for(int j=0;j<loctions.length;j++){
+           loctions[i][j]=0;
             }
         }
+
+        getShip(loctions);
 
         Scanner sc = new Scanner(System.in);
         dot.SetlocationCells(loctions);
@@ -33,59 +31,63 @@ public class SimpleDotComTestDriver {
 
 
     }
-    //确定三个长方形位置
-
-    private static boolean setLoc(int[][] loctions, int head_x, int head_y) {
-        if (loctions[head_x][head_y] == 1) {
-            return false;
-        }
-        loctions[head_x][head_y] = 1;
-        return true;
-    }
-
-    private static boolean getShip(int[][] loctions) {
-        Random r = new Random();
-        for (int i = 0; i < 3; i++) {
-            int head_x = r.nextInt(8);
-            int head_y = r.nextInt(8);
-            int dir = r.nextInt(4);
-            if (!setLoc(loctions, head_x, head_y)) {
-                return false;
-            }
-            //0-上   1-下   2-左   3-右
-            if (dir == 0) {
-                if (!setLoc(loctions, head_x, head_y + 1)) {
-                    return false;
-                }
-                if (!setLoc(loctions, head_x, head_y + 2)) {
-                    return false;
-                }
-            } else if (dir == 1) {
-                if (!setLoc(loctions, head_x, head_y - 1)) {
-                    return false;
-                }
-                if (!setLoc(loctions, head_x, head_y - 2)) {
-                    return false;
-                }
-            } else if (dir == 2) {
-                if (!setLoc(loctions, head_x - 1, head_y)) {
-                    return false;
-                }
-                if (!setLoc(loctions, head_x - 2, head_y)) {
-                    return false;
-                }
-            } else {
-                if (!setLoc(loctions, head_x + 1, head_y)) {
-                    return false;
-                }
-                if (!setLoc(loctions, head_x + 2, head_y)) {
-                    return false;
-                }
-            }
+//    检查x，y点是否可以为战舰
+    private static boolean check(int[][] loctions,int x,int y){
+        if( x>0 && x<7 && y>0 && y<7 && loctions[x][y]==0){
             return true;
+        }
+        return false;
+    }
+    //确定三个长方形
+    private static boolean tryGetOneShip(int[][] loctions) {
+        Random r = new Random();
+        int head_x = r.nextInt(7);
+        int head_y = r.nextInt(7);
+        int dir = r.nextInt(4);
+//        if (!tryGetOneShip(loctions, head_x, head_y)) {
+//            return false;
+//        }
+        //0-上   1-下   2-左   3-右
+        if (dir == 0) {
+            if(check(loctions,head_x,head_y) && check(loctions,head_x,head_y-1) && check(loctions,head_x,head_y-2)){
+                loctions[head_x][head_y]=1;
+                loctions[head_x][head_y-1]=1;
+                loctions[head_x][head_y-2]=1;
+                return true;
+            }
+        } else if (dir == 1) {
+            if(check(loctions,head_x,head_y) && check(loctions,head_x+1,head_y) && check(loctions,head_x+2,head_y)){
+                loctions[head_x][head_y]=1;
+                loctions[head_x+1][head_y]=1;
+                loctions[head_x+2][head_y]=1;
+                return true;
+            }
+        } else if (dir == 2) {
+            if(check(loctions,head_x,head_y) && check(loctions,head_x,head_y+1) && check(loctions,head_x,head_y+2)){
+                loctions[head_x][head_y]=1;
+                loctions[head_x][head_y+1]=1;
+                loctions[head_x][head_y+2]=1;
+                return true;
+            }
+        } else {
+            if(check(loctions,head_x,head_y) && check(loctions,head_x-1,head_y) && check(loctions,head_x-2,head_y)){
+                loctions[head_x][head_y]=1;
+                loctions[head_x-1][head_y]=1;
+                loctions[head_x-2][head_y]=1;
+                return true;
+            }
         }
 
         return false;
+    }
+
+    private static void getShip(int[][] loctions) {
+
+        for (int i = 0; i < 3; ) {
+            if(tryGetOneShip(loctions)){
+                i++;
+            }
+        }
     }
 }
 
@@ -93,22 +95,24 @@ public class SimpleDotComTestDriver {
 
 
 class SimpleDotCom{
-    int[] locationCells;
+    int[][] locationCells;
     int numOfHits = 0;
-    public void SetlocationCells(int[] locs){
+    public void SetlocationCells(int[][] locs){
         locationCells = locs;
     }
     public String checkYourself(String stringGuess){
-        int guess = Integer.parseInt(stringGuess);
+        String[] spilt=stringGuess.split(",");
+        int guess_x = Integer.parseInt(spilt[0]);
+        int guess_y = Integer.parseInt(spilt[1]);
         String result = "miss";
-        for(int cell : locationCells){
-            if(guess == cell){
-                result = "hint";
-                numOfHits++;
-                break;
-            }
+
+        if(locationCells[guess_x][guess_y]==1){
+            locationCells[guess_x][guess_y]=0;
+            result = "hint";
+            numOfHits++;
         }
-        if(numOfHits == 1){
+
+        if(numOfHits == 9){
             result = "kill";
         }
         System.out.println(result);
