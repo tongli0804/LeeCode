050ae891c588088f54387ece0d8b72e3236a8123
Tmint <tongli1992@hotmail.com>
2019-08-17 17:03:00 +0800

    8月17号

---
+++ src/main/java/Leecode/MultiFor.java
@@ -0,0 +1,14 @@
+package Leecode;
+
+public class MultiFor {
+    public static void main(String[] args) {
+        String[] multi ;
+        for(int x = 0;x<4;x++){
+            for(int y = 4; y>2;y--){
+
+            }
+
+        }
+
+    }
+}
--- src/main/java/Leecode/SimpleDotComTestDriver.java
+++ src/main/java/Leecode/SimpleDotComTestDriver.java
@@ -91,9 +91,6 @@ public class SimpleDotComTestDriver {
     }
 }
 
-
-
-
 class SimpleDotCom{
     int[][] locationCells;
     int numOfHits = 0;
