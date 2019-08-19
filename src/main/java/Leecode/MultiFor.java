3364b8e49eb94201ae7b8a8746bef95dc5e2dccb
Tmint <tongli1992@hotmail.com>
2019-08-19 19:10:25 +0800

    8月19号

--- src/main/java/Leecode/MultiFor.java
+++ src/main/java/Leecode/MultiFor.java
@@ -5,7 +5,11 @@ public class MultiFor {
         String[] multi ;
         for(int x = 0;x<4;x++){
             for(int y = 4; y>2;y--){
+                System.out.println(x + " " + y);
 
+            }
+            if(x == 1){
+                x++;
             }
 
         }
