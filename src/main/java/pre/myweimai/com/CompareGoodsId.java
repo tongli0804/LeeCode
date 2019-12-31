26b121e97f4d7f513fc41afafc3b91cbfae6a5dd
xuanhua <xuanhua@myweimai.com>
2019-12-31 10:39:57 +0800

    更新文件读取和写入

--- src/main/java/pre/myweimai/com/CompareGoodsId.java
+++ src/main/java/pre/myweimai/com/CompareGoodsId.java
@@ -3,19 +3,33 @@ package pre.myweimai.com;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class CompareGoodsId {
     public static void main(String[] args) {
+        String path = "C:/Users/choice/Desktop/wenjian.txt";
+        String writerpath = "C:/Users/choice/Desktop/wenjianxieru.txt";
         try {
-            BufferedReader in = new BufferedReader(new FileReader("C:/Users/choice/Desktop/wenjian.txt"));
+            BufferedReader in = new BufferedReader(new FileReader(path));//读取
+            BufferedWriter bw = new BufferedWriter(new FileWriter(writerpath));//写入
+            ArrayList<String> arrayList = new ArrayList<String>();
             String str;
 
             while ((str = in.readLine()) != null) {
                 String[] s = str.split(", ");
-                System.out.println(s);
+                //System.out.println(Arrays.asList(s));
+                for (int i=0;i< s.length;i++){
+                    String b = s[i];
+                    bw.write(s[i]+"\r\n");
+                    //System.out.println(b);
+                    bw.flush();
+
+                }
+                bw.close();
             }
-            System.out.println(str);
-        } catch (IOException e) {
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 
