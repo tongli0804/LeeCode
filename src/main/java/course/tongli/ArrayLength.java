47c9fd542a12eb4e0c57870689463b254bc763d2
dylan.ll <dylan.ll@cainiao.com>
2021-12-27 10:28:43 +0800

    7月17号

---
+++ src/main/java/course/tongli/ArrayLength.java
@@ -0,0 +1,23 @@
+package course.tongli;
+
+/**
+ * @author dylan.ll
+ * @date 2021/12/26 18:00
+ */
+public class ArrayLength {
+    public static void main(String[] args) {
+        //StringBuilder stringBuilder=new StringBuilder();
+        //StringBuilder appendStr=new StringBuilder();
+        //for(int i=0;i<100;i++){
+        //    appendStr.append("u");
+        //}
+        String abc="1231阿士大夫撒";
+        System.out.println(abc.length());
+        //for (Long i = 0L; i < Long.MAX_VALUE; i=i+1000000000) {
+        //    stringBuilder.append(appendStr.toString());
+        //    //String test=stringBuilder.toString();
+        //    //new char[i];
+        //    System.out.println(i);
+        //}
+    }
+}
